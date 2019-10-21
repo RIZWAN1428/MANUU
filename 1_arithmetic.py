fb1454e73939d0eecf43a59e34a30aef21c0ba56
RIZWAN1428 <56825168+RIZWAN1428@users.noreply.github.com>
2019-10-21 16:19:10 +0530

    Create 1_arithmetic.py

---
+++ 1_arithmetic.py
@@ -0,0 +1,10 @@
+first_number = float(input("enter a number : "))
+second_number = float(input("enter a number : "))
+add = first_number+second_number
+substraction = first_number-second_number
+multiply = first_number*second_number
+divide = first_number/second_number
+print("addition of two number is equal to : ",add)
+print("substraction of two number is equal to : ",substraction)
+print("multiplication of two number is equal to : ",multiply)
+print("division of two number is equal to : ",divide)
